8c5d9842382b049b4f75dab217d1dd42910022c4
Werapat <hellp002@gmail.com>
2023-10-22 04:36:37 +0700

    use custom button instead of build-in button

--- components/ui/dark-mode-toggle.tsx
+++ components/ui/dark-mode-toggle.tsx
@@ -3,6 +3,7 @@
 import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
+import { Button } from "./button"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
@@ -11,12 +12,12 @@ export function ModeToggle() {
   
 
   return (<>
-    <button className="background transition-all duration-1000 block dark:hidden " onClick = {()=> {setTheme('dark')}}>
+    <Button variant = 'ghost' size = 'icon' className="background transition-all duration-1000 block dark:hidden " onClick = {()=> {setTheme('dark')}}>
     <Sun className="h-10 w-10 rotate-0  dark:-rotate-90 " />
-    </button>
-    <button className="background transition-all duration-1000 hidden dark:block " onClick = {()=> {setTheme('light')}}>
+    </Button>
+    <Button variant = 'ghost' size = 'icon' className="background transition-all duration-1000 hidden dark:block " onClick = {()=> {setTheme('light')}}>
     <Moon className="h-10 w-10 rotate-90  dark:rotate-0  " />
-    </button>
+    </Button>
     </>
   )
 }
