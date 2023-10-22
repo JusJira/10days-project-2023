2456aa402ca3cfcd65851c21b1001458b2cbe65f
Werapat <hellp002@gmail.com>
2023-10-22 22:15:02 +0700

    add new scroll bar

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             enableSystem
             disableTransitionOnChange>
       <Navbar />
-      
+      <div className='min-h-screen'></div>
       </ThemeProvider>
     </main>
   )
