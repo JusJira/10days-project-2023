539387a55735991267d2421e14ed9274e4c582b8
JustinJira <jus.jira@gmail.com>
2023-10-23 22:40:05 +0700

    feat: added theme and some other stuff

---
+++ app/account/page.tsx
@@ -0,0 +1,49 @@
+import { DarkModeToggle, ModeToggle } from "@/components/ThemeToggle";
+import SignOutButton from "@/components/signOutButton";
+import { Button } from "@/components/ui/button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth/next";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default async function account() {
+  const session = await getServerSession(authOptions);
+  return (
+    <div className="relative flex min-h-full flex-col gap-3 bg-neutral-100 p-3 dark:bg-neutral-800">
+      <div className="flex h-full min-h-full items-center justify-center rounded-md bg-white p-3 dark:bg-neutral-900">
+        {session ? (
+          <div className="flex flex-col gap-3">
+            Hi {session?.user?.name}
+            <Image
+              className="rounded-full"
+              src={session?.user?.image as string}
+              alt="Profile Image"
+              width={100}
+              height={100}
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col">You are not Logged In</div>
+        )}
+      </div>
+      {session ? (
+        <div className="flex h-full  min-h-full flex-col items-center justify-center gap-3 rounded-md bg-white p-3 dark:bg-neutral-900">
+          <div>Version {process.env.VERSION}</div>
+          <div className="flex w-full justify-center">
+            <DarkModeToggle />
+          </div>
+          <div className="flex w-full justify-center">
+            <SignOutButton />
+          </div>
+        </div>
+      ) : (
+        <div className="flex h-full min-h-full items-center justify-center rounded-md bg-white p-3 dark:bg-neutral-900">
+          <Button asChild className="w-2/3 lg:w-48">
+            <Link href="/login">Login</Link>
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Label } from "./ui/label";
+import { Switch } from "./ui/switch";
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function DarkModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const handleTheme = (state: boolean) => {
+    if (state) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <div className="flex h-10 w-2/3 flex-row items-center justify-between rounded-md border-[1px] border-black bg-white px-2 shadow-2xl dark:bg-black lg:w-48">
+      <span className="sr-only">Dark mode toggle</span>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Label htmlFor="dark-mode">{theme == 'dark' ? 'Dark Mode' : 'Light Mode'}</Label>
+      <Switch
+        id="dark-mode"
+        checked={theme == "dark"}
+        onCheckedChange={(state) => handleTheme(state)}
+      />
+    </div>
+  );
+}
