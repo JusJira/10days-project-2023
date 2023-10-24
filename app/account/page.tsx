6d15d66c479621cf5d310fc82665f563aee107f4
creampiney <sorawit12535@gmail.com>
2023-10-25 00:10:14 +0700

    feat: implement profile page ui

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { Label } from "@/components/ui/label";
 
 export default async function account() {
   const {getUser} = getKindeServerSession();
@@ -39,14 +40,25 @@ export default async function account() {
       )}
       {
         (user) && (
-          <div className="grid-cols-4">
-            <div className="inline-flex basis-24 items-center justify-center min-w-24 min-h-24 w-24 h-24 md:w-24 md:h-24 overflow-hidden bg-[#C3DCE3] rounded-full">
-              {
-                (!user.picture) ? 
-                (<span className="font-large text-[#2B788B] font-[Montserrat]">{(user.given_name || "A")[0]}</span>)
-                :
-                (<img className='h-full !object-cover' src="https://nhadepso.com/wp-content/uploads/2023/03/cap-nhat-99-hinh-anh-avatar-gau-cute-de-thuong-ngo-nghinh_1.jpg"></img>)
-              }
+          <div className="flex flex-col">
+            {/* Profile Avatar / Name / Role */}
+            <div className="flex flex-col md:flex-row items-center justify-center gap-2 p-2 -m-2 mt-2 mb-2 px-3 py-2 w-full">
+              <div className="inline-flex basis-32 items-center justify-center min-w-32 min-h-32 w-32 h-32 md:w-32 md:h-32 overflow-hidden bg-[#C3DCE3] rounded-full">
+                {
+                  (!user.picture) ? 
+                  (<span className="font-large text-[#2B788B] font-[Montserrat]">{(user.given_name || "A")[0]}</span>)
+                  :
+                  (<img className='h-full !object-cover' src="https://nhadepso.com/wp-content/uploads/2023/03/cap-nhat-99-hinh-anh-avatar-gau-cute-de-thuong-ngo-nghinh_1.jpg"></img>)
+                }
+              </div>
+              <div className="flex flex-col items-center md:items-start space-y-3 md:pl-6">
+                <div className="max-w-sm text-center">
+                  <Label className="font-bold text-md break-words">[DisplayName]</Label>
+                </div>
+                <div className="max-w-sm">
+                  <Label className="text-sm break-words">[Bio]</Label>
+                </div>
+              </div>
             </div>
           </div>
         )
