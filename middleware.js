5e2d5100ad51504da2ba22e99310d677ac96b848
JustinJira <jus.jira@gmail.com>
2023-10-24 13:28:32 +0700

    fix: middleware

--- middleware.js
+++ middleware.js
@@ -9,7 +9,8 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          */
-        "/((?!api|_next/static|_next/image|favicon.ico).*)"
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+        "/"
     ]
 };
 
