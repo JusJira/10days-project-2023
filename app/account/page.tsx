0e6e32f3d3637b5f38201ea549c4a2f241784e63
JustinJira <jus.jira@gmail.com>
2023-10-24 17:07:11 +0700

    fix: db trying to find user when there is none

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,32 +1,46 @@
 import { DarkModeToggle } from "@/components/ThemeToggle";
 import SignOutButton from "@/components/signOutButton";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { LoginLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Label } from "@/components/ui/label";
 import { db } from "@/lib/db";
 import { Lightbulb } from "lucide-react";
 
 export default async function account() {
-  const { getUser } = getKindeServerSession();
+  const { getUser, isAuthenticated } = getKindeServerSession();
   const user = await getUser();
-  const dbUser = await db.user.findFirst({
-    where: {
-      id: user.id as string,
-    },
-})
-
-if (!dbUser) {
-    // create user in db
-    await db.user.create({
-      data: {
+  async function checkIfNew() {
+    const dbUser = await db.user.findFirst({
+      where: {
         id: user.id as string,
-        email: user.email as string,
       },
-    })
-}
+    });
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id as string,
+          email: user.email as string,
+        },
+      });
+    }
+    return dbUser
+  }
+  async function getUserData() {
+    if (await !isAuthenticated()){
+      return null
+    }
+    else {
+      const data = await checkIfNew()
+      return data
+    }
+  }
+
+  const dbUser = await getUserData()
+
   return (
     <div className="relative flex min-h-full flex-col gap-3 bg-neutral-100 p-3 dark:bg-neutral-800">
       <div className="flex h-full min-h-full items-center justify-center rounded-md bg-white p-3 dark:bg-neutral-900">
@@ -55,14 +69,17 @@ if (!dbUser) {
           </div>
           <div className="flex w-full justify-center">
             <div className="flex h-10 w-2/3 flex-row items-center justify-center rounded-md border-[1px] border-black bg-white px-2 shadow-2xl dark:bg-black lg:w-48">
-              <span className="flex flex-row items-center justify-center">Balance: {dbUser?.balance?.toFixed(2)} <Lightbulb size={24} strokeWidth={2} /></span>
+              <span className="flex flex-row items-center justify-center">
+                Balance: {dbUser?.balance?.toFixed(2)}{" "}
+                <Lightbulb size={24} strokeWidth={2} />
+              </span>
             </div>
           </div>
         </div>
       ) : (
         <div className="flex h-full min-h-full items-center justify-center rounded-md bg-white p-3 dark:bg-neutral-900">
           <Button asChild className="w-2/3 lg:w-48">
-            <Link href="/login">Login</Link>
+          <LoginLink className={buttonVariants()}>Sign in</LoginLink>
           </Button>
         </div>
       )}
