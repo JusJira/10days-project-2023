e813101fb0912bef05b859399764273988c1e25e
JustinJira <jus.jira@gmail.com>
2023-10-25 18:37:16 +0700

    feat: :sparkles: add,edit,delete product and product list on main page
    Now you can add, edit and delete product that you own, and also view product and their details in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import Image from 'next/image'
+import ProductBox from "@/components/productBox";
+import { db } from "@/lib/db";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const products = await db.product.findMany({
+    select: {
+      name: true,
+      id: true,
+      price: true,
+      image: true,
+    },
+  });
+  console.log(products);
   return (
-    <div className='bg-neutral-100 dark:bg-neutral-800 h-full w-full'>Main</div>
-  )
+    <div className="bg-neutral-100 dark:bg-neutral-800 h-full w-full">
+      <div className="p-8">
+        <div className="grid-container">
+          {products.map((p, index) => (
+            <ProductBox
+              key={index}
+              name={p.name}
+              image={p.image}
+              price={p.price}
+              id={p.id}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: true,
-    },
     images: {
-      domains: ["lh3.googleusercontent.com"],
+      domains: ["lh3.googleusercontent.com","res.cloudinary.com","utfs.io"],
     },
   }
   
