f1341755301e4386a2aeeaa18ca5ae05f143413a
Werapat <hellp002@gmail.com>
2023-10-25 22:36:59 +0700

    refactor : delete unnecessary code

--- app/api/editProfile/route.ts
+++ app/api/editProfile/route.ts
@@ -6,8 +6,6 @@ export async function PUT(req : NextRequest) {
     const dbUser = await getdbUser();
     if (!dbUser) return NextResponse.json({status : 204, message : "session failure"});
     const {displayName , bio} = await req.json();
-    const validation = await db.user.findFirst({where : {id : dbUser.id}});
-    if (!validation) return NextResponse.json({status : 204 , message : "this user doesn't exist!"});
     const res = await db.user.update({where : {id : dbUser.id} , data : {displayName : displayName , bio : bio}});
 
     return NextResponse.json({status : 200 , message : res});
