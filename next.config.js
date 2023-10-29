c2e3e4c945e22a91f17e427ec9af559361549e74
JustinJira <jus.jira@gmail.com>
2023-10-29 21:24:23 +0700

    build: :fire: dockerize

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
       domains: ["lh3.googleusercontent.com","res.cloudinary.com","utfs.io"],
     },
+    output: "standalone"
   }
   
   module.exports = nextConfig
