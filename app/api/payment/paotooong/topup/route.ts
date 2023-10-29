a355533e46c1ff1203114216cc93b0fe7301650c
creampiney <sorawit12535@gmail.com>
2023-10-29 15:26:33 +0700

    fix: continue topup api

--- app/api/payment/paotooong/topup/route.ts
+++ app/api/payment/paotooong/topup/route.ts
@@ -1,17 +1,50 @@
-// import { db } from "@/lib/db";
-// import { getdbUser } from "@/lib/getUser";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function POST(req : NextRequest) {
+export async function POST(req : NextRequest) {
+    const data = await req.json()
+
+    try {
+        const loginRes = await fetch("https://paotooong.thinc.in.th/v1/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: data.email,
+                password: data.password
+            }),
+        });
+
+        const loginJson = await loginRes.json()
+
+        // const payRes = await fetch(`https://paotooong.thinc.in.th/v1/wallet/pay/${process.env.COMPANT_PAOTOOONG_ID}`, {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       "Authorization" : `Bearer ${loginJson.token.accesstoken}`
+        //     },
+        //     body: JSON.stringify({
+        //         amount: data.amount
+        //     }),
+        // });
+
+        return NextResponse.json({status : 200 , message : loginJson});
+    }
+    catch (err) {
+        return NextResponse.json({status : 402 , message : "error occured"});
+    }
     
-//     const data = await req.json()
-
-//     if (!dbUser) return NextResponse.json({status : 403, message : "session failure"});
-//     // const {displayName , bio, image} = await req.json();
-//     // console.log(await req.json())
-//     const res = await db.user.update({where : {id : dbUser.id} , data : await req.json()});
-
-//     return NextResponse.json({status : 200 , message : res});
 
     
-// }
+
+    
+
+    // if (!dbUser) return NextResponse.json({status : 403, message : "session failure"});
+    // // const {displayName , bio, image} = await req.json();
+    // // console.log(await req.json())
+    // const res = await db.user.update({where : {id : dbUser.id} , data : await req.json()});
+
+    // return NextResponse.json({status : 200 , message : res});
+
+    
+}
